e1576a4038ecc65077a4616c84ab5c6d92223136
Michele Tagliabue <mikitg.michele@gmail.com>
2019-11-04 16:10:55 +0100

    Avanzamento es 2 matlab

--- 5G Systems/matlab/esercizio2.m
+++ 5G Systems/matlab/esercizio2.m
@@ -1,20 +1,29 @@
-M = 128; % size of one OFDM block (number of subcarriers)
-N = 20; % OFDM symbols you want to transmit
-L = 5; % length of the cyclic prefix
-x = round(rand(M, N)); % data to be transmitted
+clear all;
 
-y = sqrt(M) * ifft(x); % transmission
+M = 9; % size of one OFDM block (number of subcarriers)
+N = 4; % OFDM symbols you want to transmit
+L = 5; % length of the cyclic prefix
+a = round(rand(M, N)); % data to be transmitted
+
+a1 = sqrt(M) * ifft(a); % input della parallel/serial
 
 % Cyclic prefix
-z = [y(M-L+1:M,:); y]; % matrice input della conversione P/S composta da:
+PS = [a1(M-L+1:M,:); a1]; % matrice input della conversione P/S composta da:
                          % - nella prima riga le ultime 5 righe di y (blocco OFDM)
                          % - nelle rimanenti il resto di y (valori OFDM)
 
-PS = reshape(z, 1, (M + L) * N); % conversione parallel/serial
+x = reshape(PS, 1, (M + L) * N); % conversione parallel/serial
 
+w = randn(1, (M + L) * N) + 1i * randn(1, (M + L) * N); % creo il rumore (white gaussian noise)
+
+z = filter(0, 10,x); % filtro impulse response del canale
+y = z + w; % segnale grezzo presente effettivamente nel canale
 
 % Lato ricevente
-SP = reshape(PS, M+L, N); % conversione serial to parallel
+SP = reshape(y, M+L, N); % conversione serial to parallel
 zhat = SP(L+1:M+L, :); % scarto il cyclic prefix, input della DFT
 
-b = fft(zhat) / sqrt(M); % output della DFT, quindi valori ricevuti
+b = (fft(zhat)/sqrt(M)) > 0; % output della DFT, quindi valori ricevuti
+
+b == a
+
