7fc22aff96ade6fa1c89dd6905c54173a72d1830
Michele Tagliabue <mikitg.michele@gmail.com>
2020-02-02 19:43:32 +0100

    Avanzamento progetto IA

---
+++ Intelligenza Artificiale/progetto/generator.py
@@ -0,0 +1,31 @@
+import sys
+import random
+
+class BlockedQueensGenerator:
+    def __init__(self, n_queens: int, n_blocked_queens: int):
+        self.n_queens = n_queens
+        self.n_blocked_queens = n_blocked_queens
+        self.mininfile = 4
+        random.seed(10)
+
+    def generate(self): 
+        for attempt in range(1000):
+            queensorder = [ [i,j] for i in range(self.n_queens) for j in range(self.n_queens) ]
+            random.shuffle(queensorder)
+
+            rows = [ [j for j in range(self.n_queens)]  for i in range(self.n_queens) ]
+            cols = [ [j for j in range(self.n_queens)] for i in range(self.n_queens) ]
+
+            answer : Dict[int,int] = {}
+
+            for q in queensorder:
+                r = q[0]
+                c = q[1]
+                if len(rows[r]) > self.mininfile and len(cols[c]) > self.mininfile: 
+                    rows[r].remove(c)
+                    cols[c].remove(r)
+                    answer[c] = r # because old blocked queens instances are from 1 
+                    if len(answer) == self.n_blocked_queens:
+                        print("letting n = ", self.n_queens)
+                        print("letting blocks = ", answer)
+                        return answer
---
+++ Intelligenza Artificiale/progetto/queens.py
@@ -0,0 +1,87 @@
+# queens.py
+# From Classic Computer Science Problems in Python Chapter 3
+# Copyright 2018 David Kopec
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from csp import Constraint, CSP
+from generator import BlockedQueensGenerator
+
+from typing import Dict, List, Optional
+
+
+class QueensConstraint(Constraint[int, int]):
+    def __init__(self, columns: List[int]) -> None:
+        super().__init__(columns)
+        self.columns: List[int] = columns
+
+    def satisfied(self, assignment: Dict[int, int]) -> bool:
+        # q1c = queen 1 column, q1r = queen 1 row
+        for q1c, q1r in assignment.items():
+            # q2c = queen 2 column
+            for q2c in range(q1c + 1, len(self.columns) + 1):
+                if q2c in assignment:
+                    q2r: int = assignment[q2c] # q2r = queen 2 row
+                    if q1r == q2r: # same row?
+                        return False
+                    if abs(q1r - q2r) == abs(q1c - q2c): # same diagonal?
+                        return False
+        return True # no conflict
+
+def print_solutions(n_columns: int, n_rows : int,
+    solution: Dict[int, int]):
+
+    from prettytable import PrettyTable, ALL
+    prettyTable = PrettyTable(header=False,hrules=ALL)
+
+    grid: Dic[int, List[str]] = {}
+    for i in range(n_rows):
+        grid[i] = []
+        for j in range(n_columns):
+            grid[i].append("")
+
+    for queen_column in solution.keys():
+        queen_row : int = solution[queen_column] 
+
+        grid[queen_row][queen_column] = "Q%d" % (queen_column)
+        # i,y = solution[queen]
+        # grid[i,j] = "%d" % queen
+
+    for row in grid:
+        prettyTable.add_row(grid[row])
+
+    print(prettyTable)
+
+if __name__ == "__main__":
+    n_columns = 10
+    n_rows = 10
+
+    bqg = BlockedQueensGenerator(10,2)
+    blocked_queens = bqg.generate()
+
+    queens: List[int] = range(n_columns)
+    domains: Dict[int, List[int]] = {}
+
+    for column in queens:
+        if column not in blocked_queens.keys():
+            domains[column] = range(n_rows)
+        else:
+            domains[column] = [blocked_queens[column]]
+
+    csp: CSP[int, int] = CSP(queens, domains)
+    csp.add_constraint(QueensConstraint(queens))
+    solution: Optional[Dict[int, int]] = csp.backtracking_search()
+    if solution is None:
+        print("Nessuna soluzione trovata!")
+    else:
+        print_solutions(n_columns, n_rows, solution)
+        #print(solution)
