ab99c0d2abffdf23ff63bec1fc615df4d1759993
Michele Tagliabue <mikitg.michele@gmail.com>
2020-02-24 10:31:11 +0100

    Ancora commenti

--- Intelligenza Artificiale/progetto/utils/solution_printer.py
+++ Intelligenza Artificiale/progetto/utils/solution_printer.py
@@ -3,8 +3,20 @@ from prettytable import PrettyTable, ALL
 
 
 class SolutionPrinter:
+    """
+    Classe che incapsula il metodo per stampare a console una scacchiera con delle regine posizionate
+    """
     @staticmethod
     def print_solutions(n: int, queens_coord: Dict[int, int]):
+        """
+        Stampa la configurazione di regine definita in "queens_coord" su una scacchiera n x n
+        Args:
+            n: la dimensione della scacchiera
+            queens_coord: le coordinate delle regine, indicizzate per colonna
+
+        Returns:
+            Void
+        """
         pretty_table = PrettyTable(header=False, hrules=ALL)
 
         grid: Dict[int, List[str]] = {}
